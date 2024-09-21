b0ce8f89949814202f7df7a9afab27eb809180e4
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-21 05:36:13 +0300

    deleted page module.css

--- src/app/widgets/AppBar.js
+++ src/app/widgets/AppBar.js
@@ -14,10 +14,13 @@ const AppBar = () => (
     </div>
     <div className={styles.textPart + " d-flex flex-column flex-grow-1"}>
       <div className={`${styles.topBar} text-white`}>
-        <p className={`${styles.paragraph} m-0`}>CLUB SITE</p>
-        <p className={`${styles.paragraph} m-0`}><i class="bi bi-ticket-perforated"></i> TICKETS</p>
-        <p className={`${styles.paragraph} m-0`}><i className="bi bi-shop"></i> STORE</p>
-        <p className={`${styles.paragraph} m-0`}><i class="bi bi-phone-flip"></i> APP</p>
+        <div className={`${styles.topMenus} `}>
+          <p className={`${styles.paragraph} m-0`}>CLUB SITE</p>
+          <p className={`${styles.paragraph} m-0`}><i class="bi bi-ticket-perforated"></i> TICKETS</p>
+          <p className={`${styles.paragraph} m-0`}><i className="bi bi-shop"></i> STORE</p>
+          <p className={`${styles.paragraph} m-0`}><i class="bi bi-phone-flip"></i> APP</p>
+        </div>
+        <p className={`${styles.paragraph} m-0`}><i class="bi bi-person-circle"></i> LOG IN</p>
       </div>
       <div className={styles.bottomBar + " bg-dark"}>
         <p className="m-0"></p>
