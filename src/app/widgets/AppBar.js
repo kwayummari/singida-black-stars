0dfc1a9fd9c9386b90d8d8c701f7904e52b24908
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-21 04:23:36 +0300

    deleted page module.css

--- src/app/widgets/AppBar.js
+++ src/app/widgets/AppBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
-import '../../styles/appBar.scss';
+import styles from '../../styles/appBar.scss';
 
 const AppBar = () => (
-  <header className="d-flex align-items-center app-bar bg-dark">
-    <div className="logo-part">
+  <header className={`${styles.appBar} d-flex align-items-center bg-dark`}>
+    <div className={styles.logoPart}>
       <Image
         src="/images/logo.png"
         width={150}
@@ -12,12 +12,12 @@ const AppBar = () => (
         alt="Picture of the author"
       />
     </div>
-    <div className="text-part d-flex flex-column flex-grow-1">
-      <div className="top-bar bg-success text-white">
-        {/* <p className="m-0">kwayu</p> */}
+    <div className={styles.textPart + "d-flex flex-column flex-grow-1"}>
+      <div className={`${styles.topBar} bg-success text-white`}>
+        <p className="m-0">CLUB SITE</p>
       </div>
-      <div className="bottom-bar bg-dark">
-      {/* <p className="m-0">kwayu</p> */}
+      <div className={styles.bottomBar + " bg-light"}>
+      <p className="m-0">kwayu</p>
       </div>
     </div>
   </header>
