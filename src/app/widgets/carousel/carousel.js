3abc41314edb60b451d16fbf3b3407aac07b3133
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-01 12:15:03 +0300

    deleted page module.css

--- src/app/widgets/carousel/carousel.js
+++ src/app/widgets/carousel/carousel.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from '../../../styles/carousel.module.scss';
 
 const CarouselWidget = () => (
     <div className="column">
-
+        <div className={`row-md-6 row-sm-6 p-5`}>
+            <div className={`card`}>
+            <img
+              src="/images/carousel.png"
+                    alt="Picture of the logo"
+                    className={styles.carouselImage}
+            />
+            </div>
+        </div>
     </div>
 );
 
