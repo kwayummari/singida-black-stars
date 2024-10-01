f873b4b3441ca2c4ee7642b55201e7b55ffdfd36
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-01 07:45:22 +0300

    deleted page module.css

--- src/app/widgets/Footer/Footer.js
+++ src/app/widgets/Footer/Footer.js
@@ -5,13 +5,13 @@ const Footer = () => (
   <footer className="footer">
     <div className="column">
       <div className={`${styles.topFooter} row-md-6 row-sm-6`}>
-        <div className="card">
+        <div className={`${styles.topFooter} card`}>
           <div className="card-body">
           </div>
         </div>
       </div>
       <div className={`${styles.bottomFooter} row-md-6 row-sm-6`}>
-        <div className="card">
+        <div className={`${styles.bottomFooter} card`}>
           <div className="card-body">
           </div>
         </div>
