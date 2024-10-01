70258ca6cc85432d548c00c96564c01d04ff2b3c
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-01 13:57:56 +0300

    deleted page module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import styles from "./../styles/index.module.scss"
 import CarouselWidget from "./widgets/carousel/carousel";
+import Neck from "./widgets/neck/neck";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <CarouselWidget />
+      <Neck />
     </div>
   );
 }
---
+++ src/app/widgets/neck/neck.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styles from '../../../styles/neck.module.scss';
+
+const Neck = () => (
+    <div className="container">
+        <div className={`${styles.scrollableRow} row row-cols-1 row-cols-md-4 g-4`}>
+            <div className="col">
+                <div className={`${styles.imageContainer} card p-2`}>
+                    <img
+                        src="/images/carousel.png"
+                        alt="Picture of the logo"
+                        className={styles.carouselImage}
+                    />
+                    <div className={styles.imageText}>
+                        <p>Preview | CCM KIRUMBA</p>
+                    </div>
+                    <div className={styles.imageTextBelow}>
+                        <p>Club News | 9 hours ago</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className={`${styles.imageContainer} card p-2`}>
+                    <img
+                        src="/images/carousel.png"
+                        alt="Picture of the logo"
+                        className={styles.carouselImage}
+                    />
+                    <div className={styles.imageText}>
+                        <p>Preview | CCM</p>
+                    </div>
+                    <div className={styles.imageTextBelow}>
+                        <p>Club Blog | 9 hours ago</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className={`${styles.imageContainer} card p-2`}>
+                    <img
+                        src="/images/carousel.png"
+                        alt="Picture of the logo"
+                        className={styles.carouselImage}
+                    />
+                    <div className={styles.imageText}>
+                        <p>Preview | League Table</p>
+                    </div>
+                    <div className={styles.imageTextBelow}>
+                        <p>Club News | 9 hours ago</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className={`${styles.imageContainer} card p-2`}>
+                    <img
+                        src="/images/carousel.png"
+                        alt="Picture of the logo"
+                        className={styles.carouselImage}
+                    />
+                    <div className={styles.imageText}>
+                        <p>Preview | KIRUMBA</p>
+                    </div>
+                    <div className={styles.imageTextBelow}>
+                        <p>Club News | 9 hours ago</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Neck;
