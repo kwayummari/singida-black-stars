4291f485343c2402d6dd43497caeb2cab471a9c9
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-16 02:38:02 +0300

    fixed bpttom neck

--- src/app/matches/AllMatches.js
+++ src/app/matches/AllMatches.js
@@ -7,8 +7,11 @@ const AllMatches = () => {
         <div className="container">
             <div className={`row ${styles.card}`}>
                 <div className={`col-3 ${styles.part1}`}>
-                <img src="/images/nbc2.png" alt="NBC Premier League" className={styles.leagueLogo} />
-                    <p>Tue 22 Oct <br /><strong>9:45 PM</strong></p>
+                    <div className={styles.paragraphs}>
+                        <img src="/images/nbc2.png" alt="NBC Premier League" className={styles.leagueLogo} />
+                        <p>Tue 22 Oct <br /><strong>9:45 PM</strong></p>
+                    </div>
+                    <p className={styles.location}>H</p>
                 </div>
                 <div className={`col-9 ${styles.part2}`}>
                 </div>
