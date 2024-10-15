c4baff6de7c15b50a63367b5681ef84b7b9d3b48
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-15 03:58:11 +0300

    fixed bpttom neck

--- src/app/news/LatestNews.js
+++ src/app/news/LatestNews.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from 'react';
 import styles from '../../styles/Latest.module.scss';
 import ImageCard from '../widgets/ImageCardContainer/ImageCardContainer';
