89eb70a0d05ea9aac92b0adde296c73202512755
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-16 01:38:35 +0300

    fixed bpttom neck

--- src/app/matches/page.js
+++ src/app/matches/page.js
@@ -1,9 +1,14 @@
 import PageNeck from "../widgets/neck/pageNeck";
+import styles from '../../styles/Matches.module.scss';
 
 export default function News() {
   return (
     <div>
-      <PageNeck title={'Matches'}/>
+      <PageNeck title={'Matches'} />
+      <div className={styles.subNeck}>
+        <p className={styles.title}>First Team</p>
+        <p className={styles.season}>Season <strong>2024/25 <i className="bi bi-arrow-down"></i></strong></p>
+      </div>
     </div>
   );
 }
