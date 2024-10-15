b3703eae4d19219c96158723247144eb1d262943
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-16 01:48:50 +0300

    fixed bpttom neck

--- src/app/matches/page.js
+++ src/app/matches/page.js
@@ -9,6 +9,11 @@ export default function News() {
         <p className={styles.title}>First Team</p>
         <p className={styles.season}>Season <strong>2024/25 <i className="bi bi-arrow-down"></i></strong></p>
       </div>
+      <div className={styles.tabs}>
+        <p className={styles.tabHeader}>Fixtures</p>
+        <p className={styles.tabHeader}>Results</p>
+        <p className={styles.tabHeader}>Table</p>
+      </div>
     </div>
   );
 }
