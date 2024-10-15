0047fc23a81595cf1906a20abaea5e080887ea6c
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-15 05:34:09 +0300

    fixed bpttom neck

--- src/app/matches/page.js
+++ src/app/matches/page.js
@@ -1,9 +1,8 @@
-import styles from "./../../styles/index.module.scss"
 import PageNeck from "../widgets/neck/pageNeck";
 
 export default function News() {
   return (
-    <div className={styles.page}>
+    <div>
       <PageNeck title={'Matches'}/>
     </div>
   );
