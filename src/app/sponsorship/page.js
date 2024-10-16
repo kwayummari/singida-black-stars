915acde9da5c420253ffa4ff38cf39ccdfad292a
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-16 21:44:55 +0300

    fixed bpttom neck

--- src/app/sponsorship/page.js
+++ src/app/sponsorship/page.js
@@ -10,167 +10,74 @@ export default function Sponsorship() {
             </div>
             <p className={styles.footballTitle}>
                 <strong>
-                Come and watch the Reds in style during our Sky Bet League One campaign.
-                </strong>
-            </p>
-            <p className={styles.titleHistory}>
-                <strong>
-                SINGIDA BLACK STARS AND SOCIAL CLUB LTD.
+                    Come and watch the Reds in style during our Sky Bet League One campaign.
                 </strong>
             </p>
             <p className={styles.fullHistory}>
-                Monday: <strong>10am - 4:30pm</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Tuesday: <strong>9am - 4:30pm</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Wednesday: <strong>CLOSED</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Thursday: <strong>9am - 4:30pm</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Friday: <strong>9am - 4:30pm</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Saturday Matchday: <strong>10am - 4:30pm</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Sunday: <strong>CLOSED</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>By Post:</strong>Broadfield Stadium, Winfield Way, SINGIDA BLACK STARS, West Sussex, RH11 9RX.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>General Enquiries:</strong> <a href="mailto: noreply@singidablackstars.com">noreply@singidablackstars.com</a>
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Ticket Enquiries:</strong> <a href="mailto: tickets@singidablackstars.com">tickets@singidablackstars.com</a>
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Main Switchboard:</strong>01293 410 000
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Contacting The Club</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                The club receives hundreds of e-mails each season,
-                and although we do our best to reply to them all
-                promptly please be patient if you don't receive an
-                immediate response. Please be sure before e-mailing
-                us that you are contacting the relevant person at the club.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Ticket & Scout Enquiries </strong>
-            </p>
-            <p className={styles.fullHistory}>
-                Please call 01293 410 000 or email <a href="mailto:tickets@singidablackstars.com">tickets@singidablackstars.com</a> with your request.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Address Change/Data Change</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                If you have changed address recently and would like to inform
-                the club in order to update our data, please e-mail your new
-                and old details to <a href="mailto:noreply@singidablackstars.com">noreply@singidablackstars.com</a> with your request.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Work Experience/Placements</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                The club receives several applications every week
-                for work experience. It is rare for the club to be
-                able to offer work experience to supporters or
-                members of the public. If you would like to apply,
-                do so in writing only to the relevant head of department
-                at the club. We will keep all details on record
-                should an opportunity arise.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Souvenir Requests</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                If you are a collector of club memorabilia we are unable
-                to send free items. Badges, key rings, posters and other
-                such items can be purchased from the club shop at the stadium.<br /><br />
-                It is open 10am-4.30pm weekdays and on home matchdays or through the online shop 
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Charity Requests</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                The club receives many requests every week for signed merchandise
-                to help raise funds for charities and for individual causes.
-                The club only accept requests in writing on headed charity notepaper
-                or, in the case of individuals, with a supporting letter from your chosen charity,
-                attached to an email and sent by email to <a href="mailto:noreply@singidablackstars.com">noreply@singidablackstars.com</a> We can only
-                consider charity requests from organizations or individuals based within ten miles of the Stadium.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Trial Requests</strong>
-            </p>
-            <p className={styles.fullHistory}>
-            The club DOES NOT accept any requests for trials via email.
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>CLUB DIRECTORY</strong>
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Ownership :</strong> WAGMI United
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Chairman: </strong>  Preston Johnson
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Board of Directors: </strong> Ben Levin & Preston Johnson
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>General Manager: </strong> Tom Allman
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Club Secretary: </strong> Lewis Timms
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Ticket Office Manager: </strong> Katie Lampey
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Media Manager: </strong> Sam Gadsdon
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Redz Bar Manager: </strong> Rhona Kaye
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Grounds Team: </strong> Complete Turf Care 
+                We have match sponsor opportunities for our home league and cup games which
+                give you the opportunity to entertain your business clients or your friends in style.<br /><br />
+
+                We can provide you with a bespoke package to suit your needs and make it a day to remember.
             </p>
             <p className={styles.footballTitle}>
                 <strong>
-                FOOTBALL MANAGEMENT
+                    MATCH SPONSORSHIP
+                </strong>
+            </p>
+            <p className={styles.fullHistory}> <strong>Your package includes:</strong>  </p>
+            <p className={styles.fullHistory}>
+                -Admission to the Innovation Electrical Executive Suite.<br /><br />
+
+                -Up to eight hospitality tickets with a two-course meal.<br /><br />
+
+                -Team sheet and scoreboard advertising with your company logo.<br /><br />
+
+                -Watch the game from the best seats in the house in the Executive Area.<br /><br />
+
+                -PA announcements throughout the day.<br /><br />
+
+                -Social media coverage and photos on match day.<br /><br />
+
+                -Signed shirt presentation pitch side before the game with professional photographs.<br /><br />
+
+                -Choose and present the man of the match award with professional photographs.<br /><br />
+
+                The price for this package is £1200 + VAT.
+            </p>
+            <p className={styles.footballTitle}>
+                <strong>
+                    MATCH BALL SPONSORSHIP
                 </strong>
             </p>
             <p className={styles.fullHistory}>
-                <strong>First-Team Manager: </strong> Rob Elliot
+                We have sponsorship opportunities for our home league and cup
+                games which give you the opportunity to entertain your business
+                clients or your friends in style.<br /><br />
+
+                We can provide you with a bespoke package to suit your needs and make it a day to remember.
             </p>
+            <p className={styles.fullHistory}> <strong>Your package includes: </strong>  </p>
             <p className={styles.fullHistory}>
-                <strong>Assistant Manager: </strong> Louis Storey
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>First-Team Coach:  </strong> Antony Sweeney
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Goalkeeping Coach: </strong> Vacant
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>First Team Analyst: </strong> Jake Fearnley
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>First Team Physio: </strong> Tom Dudeney
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Head of Performance: </strong> Ricky McFarlane
-            </p>
-            <p className={styles.fullHistory}>
-                <strong>Kit & Equipment: </strong> Peter Reynolds 
+                Admission to the Innovation Electrical Executive Suite<br /><br />
+
+                -Up to four hospitality tickets with a two-course meal<br /><br />
+
+                -Team sheet and scoreboard advertising with your company logo.<br /><br />
+
+                -Watch the game from the best seats in the house in the Executive Area<br /><br />
+
+                -PA announcements throughout the day<br /><br />
+
+                -Social media coverage and photos on match day<br /><br />
+
+                -Signed match ball presentation pitch side before the game with professional photographs<br /><br />
+
+                The price for this package is £750 + VAT.<br /><br />
+
+                To find out more please email commercial@singidablackstars.com or call 01293 410000<br /><br />
+
+                Match sponsorship packages are always popular so get in touch now to secure
+                your place at one of our home games by contacting commercial@singidablackstars.com
             </p>
         </div>
     );
