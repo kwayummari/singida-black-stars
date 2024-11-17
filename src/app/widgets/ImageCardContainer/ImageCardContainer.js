cd337a944ba4e6da83388ab81488b88d332673aa
Kwayu Mmari <mmaridenis@gmail.com>
2024-11-18 02:43:14 +0300

    fixed data

--- src/app/widgets/ImageCardContainer/ImageCardContainer.js
+++ src/app/widgets/ImageCardContainer/ImageCardContainer.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import styles from '../../../styles/Latest.module.scss';
 
+const timeAgo = (timestamp) => {
+    const now = new Date();
+    const postDate = new Date(timestamp);
+    const diffInMs = now - postDate;
+    const diffInSecs = Math.floor(diffInMs / 1000);
+    const diffInMins = Math.floor(diffInSecs / 60);
+    const diffInHours = Math.floor(diffInMins / 60);
+    const diffInDays = Math.floor(diffInHours / 24);
+    const diffInMonths = Math.floor(diffInDays / 30);
+
+    if (diffInDays < 1) {
+        if (diffInHours < 1) {
+            return `${diffInMins} minutes ago`;
+        }
+        return `${diffInHours} hours ago`;
+    }
+
+    if (diffInDays < 30) {
+        return `${diffInDays} days ago`;
+    }
+
+    return diffInMonths === 1
+        ? '1 month ago'
+        : `${diffInMonths} months ago`;
+};
+
 const ImageCard = ({ imageSrc, altText, description, reportDate, onClick }) => (
     <div className="col-12 col-md-4" onClick={onClick}>
         <div className={`${styles.imageContainer} card`}>
@@ -14,7 +40,7 @@ const ImageCard = ({ imageSrc, altText, description, reportDate, onClick }) => (
                 <p>{description}</p>
             </div>
             <div className={styles.imageTextBelow}>
-                <p>{reportDate}</p>
+            <p>{timeAgo(reportDate)}</p>
             </div>
         </div>
     </div>
