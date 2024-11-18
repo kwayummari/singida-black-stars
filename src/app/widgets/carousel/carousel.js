808319be462c0d9e2b0cab87c3b4a37a74db2b04
Kwayu Mmari <mmaridenis@gmail.com>
2024-11-18 05:27:47 +0300

    fixed data

--- src/app/widgets/carousel/carousel.js
+++ src/app/widgets/carousel/carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '../../../styles/carousel.module.scss';
 
