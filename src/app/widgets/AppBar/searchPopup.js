7ba5cd64148eef103842f37181ed7784d5d677d8
Kwayu Mmari <mmaridenis@gmail.com>
2024-11-19 06:48:24 +0300

    fixed data

---
+++ src/app/widgets/AppBar/searchPopup.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NewsDetailsModal = ({ newsItem, onClose }) => (
+    <div className="modal" style={{ display: 'block' }}>
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title">{newsItem.title}</h5>
+                    <button type="button" className="btn-close" onClick={onClose}></button>
+                </div>
+                <div className="modal-body">
+                    <img
+                        src={newsItem.image}
+                        alt={newsItem.title}
+                        style={{ width: '100%', marginBottom: '1rem' }}
+                    />
+                    <p><strong>Category:</strong> {newsItem.categoryName}</p>
+                    <p>{newsItem.description}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default NewsDetailsModal;
